f7765c1c7b603d0879b58f2e97bf3bd9c5708e7b
Sajitha Pathirana <sajitha.pathirana@entrusttitle.com>
2021-09-05 19:26:20 +0530

    Adding enhanced type

--- src/main/java/commands/Actions.java
+++ src/main/java/commands/Actions.java
@@ -8,37 +8,44 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Actions {
     private static WebDriver driver;
-    public static WebDriver getWebDriver(){
+
+    public static WebDriver getWebDriver() {
         WebDriverManager.chromedriver().setup();
-        if(driver==null){
-            driver=new ChromeDriver();
+        if (driver == null) {
+            driver = new ChromeDriver();
         }
         return driver;
     }
-    public static void click(WebElement element,long timeOut){
-        try{
-            WaitUntil.visibility(element,timeOut);
-            WaitUntil.elementToBeClickable(element,timeOut);
+
+    public static void click(WebElement element, long timeOut) {
+        try {
+            WaitUntil.visibility(element, timeOut);
+            WaitUntil.elementToBeClickable(element, timeOut);
             element.click();
-        }catch (ElementClickInterceptedException exception){
-            log("Warning :: element was not clicked \n"+exception.getMessage());
-            for(long i=0;i<timeOut;i++){
-                try{
+        } catch (ElementClickInterceptedException exception) {
+            log("Warning :: element was not clicked \n" + exception.getMessage());
+            for (long i = 0; i < timeOut; i++) {
+                try {
                     sleep(1);
                     element.click();
                     break;
-                }catch (ElementClickInterceptedException exception1){
-                    log("Warning :: "+exception1.getMessage()+ " Try "+i);
+                } catch (ElementClickInterceptedException exception1) {
+                    log("Warning :: " + exception1.getMessage() + " Try " + i);
                 }
             }
         }
-
     }
 
-    public static void log(String message){
+    public static void type(WebElement element,String text,long timeOut){
+        WaitUntil.visibility(element,timeOut);
+        element.sendKeys(text);
+    }
+
+    public static void log(String message) {
         System.out.println(message);
     }
-    public static void sleep(long timeOut){
+
+    public static void sleep(long timeOut) {
         try {
             Thread.sleep(timeOut);
         } catch (InterruptedException e) {
--- src/test/java/commands/VerifyCommands.java
+++ src/test/java/commands/VerifyCommands.java
@@ -11,6 +11,8 @@ public class VerifyCommands {
     public void verifyClick(){
         driver=Actions.getWebDriver();
         driver.get("https://sajithatharaka.github.io/employee_registration/html-elements/delayed_html.html");
+        Actions.type(driver.findElement(By.id("fname")),"Selenium",10);
+        Actions.sleep(2);
         Actions.click(driver.findElement(By.xpath("//a[text()='Next']")),10);
     }
 
